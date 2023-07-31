c0771ed361f47f977ec6f0d01ff47ae7d6e4e1e4
script-1024 <benny20050218@gmail.com>
2023-08-01 01:16:03 +0800

    [Fix] 稍微调整，移除了不必要的依赖项

--- AppTitleBar.xaml.cs
+++ AppTitleBar.xaml.cs
@@ -1,25 +1,10 @@
-﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using System;
+using Microsoft.UI.Xaml;
 using Windows.Graphics;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace minecraft_command_helper
 {
-    public sealed partial class AppTitleBar : UserControl
+    public sealed partial class AppTitleBar : Microsoft.UI.Xaml.Controls.UserControl
     {
         public AppTitleBar()
         {
@@ -28,9 +13,9 @@ namespace minecraft_command_helper
             this.SizeChanged += OnSizeChanged;
         }
 
-        public void SetTitle(string text="", bool visible=true)
+        public void SetTitle(string text, bool visible=true)
         {
-            if (text != "") Title.Text = text;
+            if (text != null) Title.Text = text;
             if (visible) Title.Visibility = Visibility.Visible;
             else Title.Visibility = Visibility.Collapsed;
         }
@@ -45,21 +30,21 @@ namespace minecraft_command_helper
         {
             var titleBar = App.AppWindow.TitleBar;
 
-            // 当前控件的实际宽度.
+            // 当前控件的实际宽度
             var totalSpace = ActualWidth;
-            var height = ActualHeight;
+            var height = Convert.ToInt32(ActualHeight);
 
-            // 搜索框的左边界相对于整个控件左边界的偏移值.
+            // 搜索框的左边界相对于整个控件左边界的偏移值
             var searchLeftOffset = SearchBox.ActualOffset.X;
 
-            // 搜索框的右边界相对于整个控件左边界的偏移值.
+            // 搜索框的右边界相对于整个控件左边界的偏移值
             var searchRightOffset = searchLeftOffset + SearchBox.ActualWidth;
 
             var leftSpace = searchLeftOffset;
             var rightSpace = totalSpace - searchLeftOffset - SearchBox.ActualWidth;
 
-            var leftRect = new RectInt32(0, 0, Convert.ToInt32(leftSpace), Convert.ToInt32(height));
-            var rightRect = new RectInt32(Convert.ToInt32(searchRightOffset), 0, Convert.ToInt32(rightSpace), Convert.ToInt32(height));
+            var leftRect = new RectInt32(0, 0, Convert.ToInt32(leftSpace), height);
+            var rightRect = new RectInt32(Convert.ToInt32(searchRightOffset), 0, Convert.ToInt32(rightSpace), height);
 
             titleBar.SetDragRectangles(new RectInt32[] { leftRect, rightRect });
         }
